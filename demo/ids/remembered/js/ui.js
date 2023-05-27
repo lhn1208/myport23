bc4e2e0e84f2ff316a1911ab55d395688780d2d0
lhn1208 <lhn1208@naver.com>
2023-05-27 23:53:13 +0900

    포트폴리오 수정

--- demo/ids/remembered/js/ui.js
+++ demo/ids/remembered/js/ui.js
@@ -17,24 +17,24 @@ $(document).ready(function () {
 
 
     //공개여부
-    $('.register .switch').on({
+    $('.register_form .switch').on({
         click:function(e){
             // $('.register .switch').toggleClass('active');
              $(this).toggleClass('active');
         }
     });
     //프리미엄 서비스 탭
-    var item=$('.tab_area').find('button');
-    var section=$('.section_wrap section');						
-    var gnb_h=$('#remember_gnb').height();
-    item.click(function(){
-        $(this).addClass('on').siblings().removeClass('on');
-        var idx=$(this).index();
-        $('html, body').stop().animate({
-            'scrollTop': section.eq(idx).offset().top-gnb_h
-        }, 1000);													
-        return false;
-    });		
+    // var item=$('.tab_area').find('button');
+    // var section=$('.section_wrap section');						
+    // var gnb_h=$('#remember_gnb').height();
+    // item.click(function(){
+    //     $(this).addClass('on').siblings().removeClass('on');
+    //     var idx=$(this).index();
+    //     $('html, body').stop().animate({
+    //         'scrollTop': section.eq(idx).offset().top-gnb_h
+    //     }, 1000);													
+    //     return false;
+    // });		
 
     //부고 슬라이드     
     var swiper = new Swiper(".memorial_slide", {
@@ -58,33 +58,16 @@ $(document).ready(function () {
         }
     });
     // 탭메뉴            
-    // 컨텐츠 내용을 숨겨주세요!
-    var tabBtn = $("#tab_btn > ul > li"); //각각의 버튼을 변수에 저장
-    var tabCont = $("#tab_cont > div");  //각각의 컨텐츠를 변수에 저장
+    var $tab=$("#tab_menu li");
+    var $tab_cont=$(".tab_cont_wrap .tab_cont");
 
-    tabCont.hide().eq(0).show();
-
-    tabBtn.click(function(){
-    var target = $(this);        //버튼의 타켓(순서)을 변수에 저장
-    var index = target.index();  //버튼의 순서를 변수에 저장
-        tabBtn.removeClass("active");   //버튼의 클래스를 삭제
-        target.addClass("active");   //타켓의 클래스를 추가
-        tabCont.css("display","none");
-        tabCont.eq(index).css("display","block");
+    $tab.click(function(){
+        var idx=$(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $tab_cont.eq(idx).addClass('on').siblings().removeClass('on');
+        return false;
     });
 
-    var tabBtn1 = $(".tab_menu_list > ul > li"); //각각의 버튼을 변수에 저장
-    var tabCont1 = $(".subtab_content_area > div");  //각각의 컨텐츠를 변수에 저장
-    tabCont1.hide().eq(0).show();
-
-    tabBtn1.click(function(){
-    var target = $(this);        //버튼의 타켓(순서)을 변수에 저장
-    var index = target.index();  //버튼의 순서를 변수에 저장
-        tabBtn1.removeClass("active");   //버튼의 클래스를 삭제
-        target.addClass("active");   //타켓의 클래스를 추가
-        tabCont1.css("display","none");
-        tabCont1.eq(index).css("display","block");
-    });
     //faq
     $(".faq_area .header").click(function() {
         $(this).next(".content").stop().slideToggle(300);
