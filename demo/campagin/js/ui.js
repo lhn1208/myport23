3874ca7d91fabbdfc7a37c25f424232c909ea625
lhn1208 <lhn1208@naver.com>
2023-07-05 14:06:21 +0900

    캠페인 컨텐츠 업로드

--- demo/campagin/js/ui.js
+++ demo/campagin/js/ui.js
@@ -61,6 +61,80 @@ $(document).ready(function(){
         $mobile_pop.find('.close').click(function(){
             $mobile_pop.removeClass('visible');
         });
-        
+        //content
+       var $gallerycont= $('.gallery_container');
+       var $btn_like=$gallerycont.find('.like');
+       $btn_like.click(function(){
+        $(this).toggleClass('on');
+       })
+       //right menu
+       var $cont_inner=$gallerycont.find('.tab_cont1 .content_inner');
+       var $contArea=$cont_inner.find('.content_area');
+       var $rightMenu=$cont_inner.find('.menu_wrap');
+       var $section = $cont_inner.find('.scroll_sec');
+       var $menuitem= $rightMenu.find('.menu li');
+       function scrollMenu(){
+           var cont_offset= $cont_inner.offset().top;
+           var scT = $(window).scrollTop();	
+           var contH= $contArea.outerHeight();//cont height
+           var menuH= $rightMenu.outerHeight();//menu height
+           var calc =  contH - menuH;
+            if (scT > cont_offset && scT < calc) {
+                $rightMenu.addClass('fixed').removeAttr('style');
+            } else if (scT >= calc) {
+                $rightMenu.removeClass('fixed').css({'position': 'absolute', 'top': calc + 'px', 'left': '50%', 'margin-left': '272px'});
+            } else {
+                $rightMenu.removeClass('fixed').removeAttr('style');
+            }
+       }
+       $menuitem.click(function(e){
+            e.preventDefault();
+            var idx=$(this).index();
+            $(this).addClass('active').siblings().removeClass('active');
+            $('html, body').stop().animate({
+                'scrollTop': $section.eq(idx).offset().top -200
+            }, 800);
+       })
+       //tab
+       $gallerycont.find('.tab_area a').click(function(e){
+            e.preventDefault();
+            $gallerycont.find('[class^="tab_cont"]').removeClass('active');
+            $($(this).attr("href")).addClass('active');
+            if ($gallerycont.find('.tab_cont1.active').length > 0) {
+                $gallerycont.find('.tab_cont1').scrollTop(0);
+                $rightMenu.removeAttr('style');
+            }
+       })
+       $gallerycont.find('.ico_links a').click(function(e){
+            e.preventDefault();
+            $('html, body').stop().animate({
+                'scrollTop': $($(this).attr("href")).offset().top
+            }, 800);
+       })
+       //layer
+       var $btnClose=$gallerycont.find('.pop_layer .btn_close');
+       var $btnCopy=$gallerycont.find('.btn_copy');
+       var $btnInfrom=$gallerycont.find('.btn_inform');
+       $btnCopy.click(function(){
+            $gallerycont.find('.pop_copy_wrap').show();
+       });
+       $btnInfrom.click(function(){
+            $($(this).attr("href")).show();
+       })
+       $btnClose.click(function(){
+            $(this).parents('.pop_cover').hide();
+       });
+       $(document).mouseup(function (e){
+            var LayerPopup = $(".pop_cover");
+            if(LayerPopup.has(e.target).length === 0){
+            LayerPopup.hide();
+        }
+        });
+       $(window).scroll(function(){  
+            if($cont_inner.length > 0){
+                scrollMenu();
+            }
+       });
+   
     })
 });
